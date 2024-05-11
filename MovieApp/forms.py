90eb882ee576df96a8dc45c7d026b11c908044b6
Sidharthkk06 <sidharthkishor.k@gmail.com>
2024-05-11 16:06:48 +0530

    Initial coomit

--- MovieApp/forms.py
+++ MovieApp/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(label='', max_length=30, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(label='', max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    email = forms.EmailField(label='', max_length=254, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    username = forms.CharField(label='', max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
     class Meta:
         model = User
-        fields = ['username','first_name','last_name','email','password1','password2',]
+        fields = ['first_name','last_name','email','username','password1','password2',]
 
 class SignInForm(forms.Form):
     username = forms.CharField(label='Username or Email', max_length=254)
